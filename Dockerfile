1cb9542af685f57fe62c337b6fbb068046408cf6
Martin Moon <martin@chequer.io>
2023-07-16 20:46:55 +0900

    fix(ci/cd) To enable textflie-collector.

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ENV DEBIAN_FRONTEND="noninteractive"
 RUN apt-get update -y && apt-get install python3-pip -y
 COPY .build/node_exporter /bin/node_exporter
 COPY infra/requirments.txt .
-COPY textfile-collector /etc/prometheus/textfile-collect
+COPY textfile-collector /etc/prometheus/textfile-collector
 
 EXPOSE      9100
 USER        nobody
--- promehteus-install.sh
+++ promehteus-install.sh
@@ -13,5 +13,6 @@ helm upgrade -i prometheus-stack prometheus-community/kube-prometheus-stack  \
 --set prometheus-node-exporter.image.repository="seongjumoon/node-exporter" \
 --set prometheus-node-exporter.image.tag="v1.6.0-ubuntu2004.0" \
 --set prometheus-node-exporter.image.pullPolicy="IfNotPresent" \
+--set prometheus-node-exporter.extraArgs[0]="--collector.textfile.directory=/etc/prometheus/textfile-collector" \
 --namespace=monitoring \
 --create-namespace
