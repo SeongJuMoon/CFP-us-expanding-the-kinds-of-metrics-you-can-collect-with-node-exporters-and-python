2b669f5f05a216dc6a90994efca443b64bef4e5e
Martin Moon <martin@chequer.io>
2023-07-16 20:58:43 +0900

    fix(ci/cd) Change ImagePullPolicy `IfNotPresent` to `Always`

--- promehteus-install.sh
+++ promehteus-install.sh
@@ -12,7 +12,7 @@ helm upgrade -i prometheus-stack prometheus-community/kube-prometheus-stack  \
 --set prometheus-node-exporter.image.registry="index.docker.io" \
 --set prometheus-node-exporter.image.repository="seongjumoon/node-exporter" \
 --set prometheus-node-exporter.image.tag="v1.6.0-ubuntu2004.0" \
---set prometheus-node-exporter.image.pullPolicy="IfNotPresent" \
+--set prometheus-node-exporter.image.pullPolicy="Always" \
 --set prometheus-node-exporter.extraArgs[0]="--collector.textfile.directory=/etc/prometheus/textfile-collector" \
 --namespace=monitoring \
 --create-namespace
